2233fcd8e1efc3ec2f1fbe80173cb1903c1c68a2
camposyuri <yurinapoleao@gmail.com>
2020-11-04 09:42:05 -0300

    [ADD] - Create POST route

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,4 +5,18 @@ module.exports = {
     const results = await knex("users");
     return response.json(results);
   },
+
+  async create(request, response, next) {
+    const { username } = request.body;
+
+    try {
+      await knex("users").insert({
+        username,
+      });
+
+      return response.status(201).send();
+    } catch (error) {
+      next(error);
+    }
+  },
 };
