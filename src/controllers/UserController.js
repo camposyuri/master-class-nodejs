a48ec9756773cbfed0fc2b67b1512b8263d5e682
camposyuri <yurinapoleao@gmail.com>
2020-11-04 10:05:52 -0300

    [ADD] - Finishing CRUD users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,10 +2,11 @@ const knex = require("../database");
 
 module.exports = {
   async index(request, response) {
+    const { id } = request.params;
+
     const results = await knex("users");
     return response.json(results);
   },
-
   async create(request, response, next) {
     const { username } = request.body;
 
@@ -15,6 +16,31 @@ module.exports = {
       });
 
       return response.status(201).send();
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async update(request, response, next) {
+    try {
+      const { username } = request.body;
+      const { id } = request.params;
+
+      await knex("users").update({ username }).where({ id });
+
+      return response.send();
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async delete(request, response, next) {
+    try {
+      const { id } = request.params;
+
+      await knex("users").where({ id }).del();
+
+      return response.send();
     } catch (error) {
       next(error);
     }
--- src/server.js
+++ src/server.js
@@ -5,6 +5,14 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+// not found
+app.use((request, response, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+
+  next(error);
+});
+
 // catch all
 app.use((error, request, response, next) => {
   response.status(error.status || 500);
